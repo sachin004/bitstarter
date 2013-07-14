089629ed2c702ebf62101ae5f1874fba2c30697d
Ubuntu <ubuntu@ip-172-31-32-104.us-west-2.compute.internal>
2013-07-14 19:38:07 +0000

    I hoe it works

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', function(err, data){
       if(err) throw err;
-console.log(data);
+response.send(data);
 
 });
 });
